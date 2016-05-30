a66877d99121ecc029dee54e4bd195f24625191f
Jason Watson <jason.jw@live.co.uk>
2016-05-30 15:15:19 +0100

    Adjusted for new project oxpytools

--- licenses/LICENSE.rst
+++ licenses/LICENSE.rst
@@ -1,4 +1,4 @@
-Copyright (c) year, author
+Copyright (c) 2016, Jason Watson
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
--- oxpytools/__init__.py
+++ oxpytools/__init__.py
@@ -1,7 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 """
-This is an Astropy affiliated package.
+oxpytools - A GCT Python project utilising ctapipe and targetio
 """
 
 # Affiliated packages may add whatever they like to this file, but
