1ac640b75964ccadfcaf9e21d4e63f6fa980d50a
Jason Watson <jason.jw@live.co.uk>
2016-06-03 16:55:00 +0100

    Method to obtain path to datasets file

---
+++ oxpytools/utils/datasets.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from astropy import log
+
+def get_oxpytools_extra_path(filename):
+    """Get path to a dataset file inside `oxpytools-extra`.
+    """
+
+    import oxpytools
+    path = Path(oxpytools.__file__).parent.parent.joinpath('oxpytools-extra')
+    dataset = Path(path.joinpath('datasets').joinpath(filename))
+    try:
+        if not dataset.exists():
+            raise FileNotFoundError
+    except FileNotFoundError as e:
+        log.exception("file path does not exist: '{}'".format(dataset))
