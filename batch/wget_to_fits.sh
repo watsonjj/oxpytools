fdbba357bdf8431e65a9a452948b0fa1ad8a3363
Jason Watson <jason.jw@live.co.uk>
2016-08-02 20:27:22 +0100

    Updated write_img_fits to store MC information

--- batch/wget_to_fits.sh
+++ batch/wget_to_fits.sh
@@ -1,27 +1,27 @@
-#!/bin/bash
+	#!/bin/bash
 
 proton() {
 	directory=http://cta.cppm.in2p3.fr/MeudonPrototype/simtel/simtel_proton_30deg_r1000/
-	name=simtel_runmeudon_proton_30tel_30deg_$1.gz
+	name=simtel_runmeudon_proton_30tel_30deg_$1
 	local_directory=~/Software/outputs/sim_telarray/meudon_proton/
 	cd ${local_directory}
-	if [ ! -f ${name} ]; then
-	    wget ${directory}${name} &>${local_directory}${name}_progress.txt
-	fi
+#	if [ ! -f ${name} ]; then
+#	    wget ${directory}${name} &>${local_directory}${name}_progress.txt
+#	fi
 	source activate cta
-	python ~/Software/OxPyTools/bin/write_img_fits -f ${local_directory}${name} -o ${local_directory}${name}.fits &>${local_directory}${name}_progress.txt
+	python ~/Software/oxpytools/scripts/write_img_fits -f ${local_directory}${name}.gz -o ${local_directory}${name}.fits &>${local_directory}${name}_progress.txt
 }
 export -f proton
 gamma() {
 	directory=http://cta.cppm.in2p3.fr/MeudonPrototype/simtel/simtel_gamma_30deg/
-	name=simtel_runmeudon_gamma_30tel_30deg_$1.gz
+	name=simtel_runmeudon_gamma_30tel_30deg_$1
 	local_directory=~/Software/outputs/sim_telarray/meudon_gamma/
 	cd ${local_directory}
-	if [ ! -f ${name} ]; then
-	    wget ${directory}${name} &>${local_directory}${name}_progress.txt
-	fi
+#	if [ ! -f ${name} ]; then
+#	    wget ${directory}${name} &>${local_directory}${name}_progress.txt
+#	fi
 	source activate cta
-	python ~/Software/OxPyTools/bin/write_img_fits -f ${local_directory}${name}  -o ${local_directory}${name}.fits  &>${local_directory}${name}_progress.txt
+	python ~/Software/oxpytools/scripts/write_img_fits -f ${local_directory}${name}.gz  -o ${local_directory}${name}.fits  &>${local_directory}${name}_progress.txt
 }
 export -f gamma
 
